e1dbd8deee13211a8c769a083e73d89c1339d93a
Ahmadrezadl <ahmadrezakml@gmail.com>
2025-03-18 16:58:50 +0330

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.use(express.json());
 app.post('/verify-purchase', require('./routes/verifyPurchase'));
 
 const PORT = process.env.PORT || 3000;
+
+
+console.log("Routes Loaded");
+console.log(app._router.stack.map(r => r.route && r.route.path));
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
